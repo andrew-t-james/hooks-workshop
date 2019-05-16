c0a2804d0dfe305da2a2c3008a5fa359e822ff2b
Andrew James <ajames20@gmail.com>
2019-05-16 13:59:53 -0700

    Add local storage solution

--- lessons/04-effects/exercise/NewPost.js
+++ lessons/04-effects/exercise/NewPost.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useRef } from "react"
 import { FaDumbbell } from "react-icons/fa"
 
@@ -11,12 +12,17 @@ const MAX_MESSAGE_LENGTH = 200
 
 export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
   const [{ auth }] = useAppState()
-  const [message, setMessage] = useState("Ran around the lake.")
+  const storageKey = makeNewPostKey(date)
+  const [message, setMessage] = useState(getLocalStorageValue(storageKey) || "")
   const messageTooLong = message.length > MAX_MESSAGE_LENGTH
 
   function handleMessageChange(event) {
     setMessage(event.target.value)
   }
+
+  useEffect(() => {
+    setLocalStorage(storageKey, message)
+  }, [message, storageKey])
 
   return (
     <div className={"NewPost" + (messageTooLong ? " NewPost_error" : "")}>
